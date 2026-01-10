13b4fd9ab757fe100bde73f28dffad062681de10
ShresthaNaskar.silversky <dhruv156328@gmail.com>
2026-01-10 14:19:13 +0530

    modify in section_9

--- SQL/Basics of SQL/section_9.sql
+++ SQL/Basics of SQL/section_9.sql
@@ -134,3 +134,24 @@ FROM
   customers 
 WHERE 
   creditLimit > 0;
+
+/* --- GENERATED COLOUMN --- */
+DROP TABLE IF EXISTS contacts;
+
+CREATE TABLE contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50) NOT NULL,
+    last_name VARCHAR(50) NOT NULL,
+    fullname varchar(101) GENERATED ALWAYS AS (CONCAT(first_name,' ',last_name)),
+    email VARCHAR(100) NOT NULL
+);
+
+ALTER TABLE products
+ADD COLUMN stockValue DEC(10,2)
+GENERATED ALWAYS AS (buyprice*quantityinstock) STORED;
+
+SELECT 
+    productName, 
+    ROUND(stockValue, 2) stock_value
+FROM
+    products;
