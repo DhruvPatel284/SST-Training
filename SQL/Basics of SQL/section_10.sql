bcbee70f7b2c8cca83d1861aa42d3f9472edf6ef
ShresthaNaskar.silversky <dhruv156328@gmail.com>
2026-01-12 10:20:01 +0530

    section_10 complete

--- SQL/Basics of SQL/section_10.sql
+++ SQL/Basics of SQL/section_10.sql
@@ -68,6 +68,8 @@ CREATE TABLE products(
         ON DELETE CASCADE
 ) ENGINE=INNODB;
 
+show create table products ;
+
 INSERT INTO products(productName, categoryId)
 VALUES
     ('iPhone', 1), 
@@ -82,4 +84,87 @@ SET categoryId = 100
 WHERE categoryId = 1;
 
 SELECT * FROM categories;
+
+DELETE FROM categories
+WHERE categoryId = 2;
+
+/* --- NOT NULL --- */
+CREATE TABLE tasks (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(255) NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE
+);
+
+DESC tsaks;
+
+/* --- UNIQUE --- */
+CREATE TABLE suppliers (
+    supplier_id INT AUTO_INCREMENT,
+    name VARCHAR(255) NOT NULL,
+    phone VARCHAR(15) NOT NULL UNIQUE,
+    address VARCHAR(255) NOT NULL,
+    PRIMARY KEY (supplier_id),
+    CONSTRAINT uc_name_address UNIQUE (name,address)
+);
+
+INSERT INTO suppliers(name, phone, address) 
+VALUES( 'ABC Inc', 
+       '(408)-908-2476',
+       '4000 North 1st Street');
+
+INSERT INTO suppliers(name, phone, address) 
+VALUES( 'XYZ Corporation','(408)-908-2476','3000 North 1st Street');
+
+/* --- CHECK --- */
+CREATE TABLE parts (
+    part_no VARCHAR(18) PRIMARY KEY,
+    description VARCHAR(40),
+    cost DECIMAL(10,2 ) NOT NULL CHECK (cost >= 0),
+    price DECIMAL(10,2) NOT NULL CHECK (price >= 0)
+);
+
+SHOW CREATE TABLE parts;
+
+INSERT INTO parts(part_no, description,cost,price) 
+VALUES('A-001','Cooler',0,-100);
+
+DROP TABLE IF EXISTS parts;
+
+CREATE TABLE parts (
+    part_no VARCHAR(18) PRIMARY KEY,
+    description VARCHAR(40),
+    cost DECIMAL(10,2 ) NOT NULL CHECK (cost >= 0),
+    price DECIMAL(10,2) NOT NULL CHECK (price >= 0),
+    CONSTRAINT parts_chk_price_gt_cost 
+        CHECK(price >= cost)
+);
+
+SHOW CREATE TABLE parts;
+INSERT INTO parts(part_no, description,cost,price) 
+VALUES('A-001','Cooler',200,100);
+
+/* --- DEFAULT --- */
+CREATE TABLE cart_items 
+(
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL,
+    price DEC(5,2) NOT NULL,
+    sales_tax DEC(5,2) NOT NULL DEFAULT 0.1,
+    CHECK(quantity > 0),
+    CHECK(sales_tax >= 0) 
+);
+INSERT INTO cart_items(name, quantity, price)
+VALUES('Keyboard', 1, 50);
+
+INSERT INTO cart_items(name, quantity, price, sales_tax)
+VALUES('Battery',4, 0.25 , DEFAULT);
+
+ALTER TABLE cart_items
+ALTER COLUMN quantity SET DEFAULT 1;
+
+SELECT * FROM cart_items;
+
+
 
