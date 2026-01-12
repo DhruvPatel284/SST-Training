03c9f8bafcd8df6f937290184b0e84ae196de4f9
ShresthaNaskar.silversky <dhruv156328@gmail.com>
2026-01-12 13:52:39 +0530

    task 1 completed

---
+++ SQL/Practice SQL/SQL TASK/sql_task_1.sql
@@ -0,0 +1,33 @@
+
+use sql_task;
+/* Tasks 1 */
+
+--1) Get all users.
+ select * from users;
+
+--2) Find all products with price greater than 1000 // 100.
+ select* from products where price > 100;
+
+--3) Show only name and email from users table.
+ select name , email from users ;
+
+--4) Get all products with stock less than 10 // 200.
+ select * from products where stock < 200 ;
+ 
+--5) Find users from a specific city (e.g., "Delhi").
+ select * from users where city = 'Delhi';
+
+--6) Sort products by price (ascending).
+ select * from products order by price ;
+
+--7) Sort users by created_at (latest first).
+ select * from users order by created_at DESC;
+
+--8) Count total number of users.
+ select count(*) from users ;
+
+--9) Find minimum and maximum product price.
+ select min(price), max(price) from products;
+
+--10) Get distinct product categories.
+ select distinct(category) from products;
