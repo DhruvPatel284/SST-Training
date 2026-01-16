9d91ac9933123496cebbec99f5dc34f9b0dd809b
ShresthaNaskar.silversky <dhruv156328@gmail.com>
2026-01-16 14:57:43 +0530

    Complete Section 6

--- Nest js Udemy/Section 5/messages/src/main.ts
+++ Nest js Udemy/Section 5/messages/src/main.ts
@@ -4,11 +4,6 @@ import { MessagesModule } from './messages/messages.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(MessagesModule);
-  app.use((req, res, next) => {
-    res.removeHeader('x-powered-by');
-    res.removeHeader('date');
-    next();
-  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3000);
 }
