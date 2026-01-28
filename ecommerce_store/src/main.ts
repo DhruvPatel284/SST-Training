00c35e8056b66afbf6040b27a9d551b58c346174
Dhruv Patel <dhruv156328@gmail.com>
2026-01-28 13:50:11 +0530

    solved session problem by storing session in sqlite

--- ecommerce_store/src/main.ts
+++ ecommerce_store/src/main.ts
@@ -1,11 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import session from 'express-session';
+import SQLiteStore from 'connect-sqlite3';
+
+const SQLiteStoreSession = SQLiteStore(session);
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
     app.use(
     session({
+      name: 'sid',
+      store: new SQLiteStoreSession({
+        db: 'sessions.sqlite',
+        dir: './',
+      }),
       secret: 'secret-key', 
       resave: false,
       saveUninitialized: false,
